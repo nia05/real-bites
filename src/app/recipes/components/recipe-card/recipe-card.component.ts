67f4d77f8c4aeaaf9d22110e2704198db91709aa
Marina <marinajocovic@gmail.com>
2025-02-24 11:36:19 +0100

    update recipe card component

--- src/app/recipes/components/recipe-card/recipe-card.component.ts
+++ src/app/recipes/components/recipe-card/recipe-card.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { Recipe } from '../../../../shareable/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-card',
-  imports: [],
+  imports: [ MatCardModule, CommonModule, MatIconModule ],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss'
 })
-export class RecipeCardComponent {
+export class RecipeCardComponent implements OnChanges {
+    @Input() recipe!: Recipe;
 
+    test = true;
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['recipe']?.currentValue) {
+           this.test = false;
+        }
+    }
 }
