c55b1d80967adc89dcdc367159a0afb3ae5dac40
Marina <marinajocovic@gmail.com>
2025-02-24 19:20:24 +0100

    add recipe detail page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { RecipesOverviewPageComponent } from './features/recipes/pages/recipes-overview-page/recipes-overview-page.component';
+import { RecipeDetailPageComponent } from './features/recipes/pages/recipe-detail-page/recipe-detail-page.component';
 
 export const routes: Routes = [
     { path: 'recipes', component: RecipesOverviewPageComponent},
+    { path: 'recipe/:ID', component: RecipeDetailPageComponent},
     { path: '**', redirectTo: 'recipes', pathMatch: 'full'}
 ];
---
+++ src/app/features/recipes/pages/recipe-detail-page/recipe-detail-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail-page',
+  imports: [],
+  templateUrl: './recipe-detail-page.component.html',
+  styleUrl: './recipe-detail-page.component.scss'
+})
+export class RecipeDetailPageComponent {
+
+}
