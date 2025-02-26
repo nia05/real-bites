81183f74ba532f2d11da101591a5b3e804688d0d
Marina <marinajocovic@gmail.com>
2025-02-26 16:22:28 +0100

    use breadcrumb service in recipes update page

--- src/app/features/recipes/pages/recipes-update-page/recipes-update-page.component.ts
+++ src/app/features/recipes/pages/recipes-update-page/recipes-update-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+import { BreadcrumbComponent } from '../../../../shareable/components/breadcrumb/breadcrumb.component';
+
+import { Breadcrumb } from '../../../../shareable/models/breadcrumb.model';
+
+import { BreadcrumbService } from '../../../../shareable/services/breadcrumb/breadcrumb.service';
+
 @Component({
-  selector: 'app-recipes-update-page',
-  imports: [],
-  templateUrl: './recipes-update-page.component.html',
-  styleUrl: './recipes-update-page.component.scss'
+    selector: 'app-recipes-update-page',
+    imports: [BreadcrumbComponent],
+    templateUrl: './recipes-update-page.component.html',
+    styleUrl: './recipes-update-page.component.scss'
 })
 export class RecipesUpdatePageComponent {
+    breadcrumbList!: Breadcrumb[];
 
+    constructor(private breadcrumbService: BreadcrumbService) {}
+
+    ngOnInit(): void {
+        this.breadcrumbService.setBreadcrumbs('recipe-create');
+        this.breadcrumbList = this.breadcrumbService.getBreadcrumbs();
+    }
 }
