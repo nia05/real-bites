cc36fc0e1afe348697e749a32e82bf8400c0e871
Marina <marinajocovic@gmail.com>
2025-02-26 16:00:28 +0100

    add breadcrumb component to report create page

--- src/app/features/recipes/pages/recipe-create-page/recipe-create-page.component.ts
+++ src/app/features/recipes/pages/recipe-create-page/recipe-create-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Breadcrumb } from '../../../../shareable/models/breadcrumb.model';
+
+import { BreadcrumbComponent } from '../../../../shareable/components/breadcrumb/breadcrumb.component';
+
 @Component({
-  selector: 'app-recipe-create-page',
-  imports: [],
-  templateUrl: './recipe-create-page.component.html',
-  styleUrl: './recipe-create-page.component.scss'
+    selector: 'app-recipe-create-page',
+    imports: [BreadcrumbComponent],
+    templateUrl: './recipe-create-page.component.html',
+    styleUrl: './recipe-create-page.component.scss'
 })
 export class RecipeCreatePageComponent {
-
+    breadcrumbList: Breadcrumb[] = [
+        {label: 'Recipes', route: '/recipes'},
+        {label: 'Create New Recipe', route: '/recipes/create'},
+    ];
 }
