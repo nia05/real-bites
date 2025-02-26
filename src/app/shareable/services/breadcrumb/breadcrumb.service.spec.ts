4b1659d0403d39d5e50120ad16327b0fd6583be4
Marina <marinajocovic@gmail.com>
2025-02-26 13:26:25 +0100

    breadcrumb service

---
+++ src/app/shareable/services/breadcrumb/breadcrumb.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BreadcrumbService } from './breadcrumb.service';
+
+
+describe('BreadcrumbService', () => {
+  let service: BreadcrumbService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(BreadcrumbService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
